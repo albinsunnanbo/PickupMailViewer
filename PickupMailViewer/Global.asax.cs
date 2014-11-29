7c9f498508163fd11085d1d190027bd3f8ca9f77
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2014-11-29 09:43:10 +0100

    Precahe existing mailmessages on startup

--- PickupMailViewer/Global.asax.cs
+++ PickupMailViewer/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PickupMailViewer.Helpers;
+using PickupMailViewer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -18,6 +21,16 @@ namespace PickupMailViewer
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             MailWatcher.Init();
+            Task.Factory.StartNew(FillCache);
+        }
+
+        private void FillCache()
+        {
+            var mailPaths = MailHelper.ListMailFiles(Properties.Settings.Default.MailDir);
+            foreach (var mailPath in mailPaths.Reverse()) // Fill cache from reverse to not run from the same direction as HomeController.GetMailListModel
+            {
+                MailHelper.ReadMessage(mailPath);
+            }
         }
     }
 }
