4e89aa73766bc7f56fe6850608bf7c2bc1e344b1
Anders Abel <anders@abel.nu>
2015-03-20 21:50:34 +0100

    Load sms messages at initial load.

---
+++ PickupMailViewer/Helpers/SmsHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace PickupMailViewer.Helpers
+{
+    public static class SmsHelper
+    {
+        public static IEnumerable<string> ListSmsFiles(string path)
+        {
+            return Directory.EnumerateFiles(path, "*.sms");
+        }
+    }
+}
